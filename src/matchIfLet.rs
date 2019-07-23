430d1eea84d2bc51de0d98df3d9273082044864a
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2019-07-23 14:54:20 -0700

    untill chapter 6

---
+++ src/matchIfLet.rs
@@ -0,0 +1,49 @@
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn main(){
+
+    value_in_cents(Coin::Quarter(UsState::Alaska) );
+
+    //match staement
+    let coin = Coin::Dime;
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+    //if let statement with  else for above match statement
+    let coin1 = Coin::Quarter(UsState::Alabama);
+    let mut count1 = 0;
+    if let Coin::Quarter(state1) = coin1 {
+        println!("State quarter from {:?}!", state1);
+    } else {
+        count1 += 1;
+    }
+
+
+}
